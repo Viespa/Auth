a751a07c5add64ea761709015aabbea48bbded8b
Viespa <victorep105@gmail.com>
2024-01-21 21:13:07 +0100

    Create Api

---
+++ api/HomeDb.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace api
+
+{
+    public class HomeDb : DbContext
+    {
+      
+        public HomeDb(DbContextOptions<HomeDb> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users => Set<User>();
+    }
+    
+}
---
+++ api/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace api
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
