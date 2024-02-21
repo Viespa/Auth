4275ba47e52a90bd6e5b0a479c3c6c24013a1f19
Viespa <victorep105@gmail.com>
2024-02-21 20:31:41 +0100

    bd conecction and register api

---
+++ api/Database.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SqlClient;
+using DotNetEnv;
+
+namespace api
+{
+    public class Database
+    {
+        public static string ConnectionString { get; private set; }
+
+        static Database()
+        {
+            Env.Load();
+            string ConnectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+        }
+
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING"));
+            connection.Open();
+            return connection;
+        }
+
+        public static bool TestConnection()
+        {
+            SqlConnection connection = null;
+            try
+            {
+                Env.Load();
+                connection = new SqlConnection(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING"));
+                connection.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error de conexión: " + ex.Message);
+                return false;
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
--- api/HomeDb.cs
+++ api/HomeDb.cs
@@ -6,10 +6,8 @@ namespace api
     public class HomeDb : DbContext
     {
       
-        public HomeDb(DbContextOptions<HomeDb> options) : base(options)
-        {
-
-        }
+        public HomeDb(DbContextOptions<HomeDb> options) 
+            : base(options){ }
         public DbSet<User> Users => Set<User>();
     }
     
