f40f3d899e881e6fd951944a323b96996994ffee
Viespa <victorep105@gmail.com>
2024-05-07 22:18:05 +0200

    Create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server";
 
+import bcrypt from 'bcryptjs';
 import * as z from 'zod';
 
+import { db } from '@/lib/db'; 
 import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async ( values: z.infer<typeof RegisterSchema> ) => {
    const validateFields = RegisterSchema.safeParse( values ); 
@@ -11,5 +14,25 @@ export const register = async ( values: z.infer<typeof RegisterSchema> ) => {
        return { error: 'Invalid fields' };
    }
    
-   return { success: 'Register success' };
+   const { name , email, password } = validateFields.data;
+   const hashedPassword = await bcrypt.hash( password, 10 );
+
+   const existingUser = await getUserByEmail( email );
+
+    if ( existingUser ) {
+        return { error: 'Email already exists' };
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    });
+
+
+    // TODO: Send email confirmation
+
+   return { success: 'User created' };
 }
